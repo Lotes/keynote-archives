00636d8dccd2d26d52097f6e398e8fd97cbd99bc
Markus  Rudolph <markhoernchen@gmx.de>
2023-04-12 20:54:39 +0200

    Add Tag line

--- gh-pages/src/pages/index.tsx
+++ gh-pages/src/pages/index.tsx
@@ -1,9 +1,11 @@
 import { DropZone } from "@/components/DropZone";
 import { useEffect, useState } from "react";
+import Image from "next/image";
+import Head from "next/head";
 
 export default function Home() {
-  const [name, setName] = useState<string|undefined>();
-  const [url, setUrl] = useState<string|undefined>();
+  const [name, setName] = useState<string | undefined>();
+  const [url, setUrl] = useState<string | undefined>();
 
   useEffect(() => {
     setUrl(localStorage.getItem("url") ?? undefined);
@@ -16,21 +18,42 @@ export default function Home() {
   }, [url]);
 
   return (
-    url
-      ? 
-      <main className="flex min-h-screen flex-col items-center justify-start p-24">
-        {name}
-        <button onClick={() => {
-          localStorage.setItem('name', undefined!);
-          localStorage.setItem('url', undefined!);
-          setName(undefined);
-          setUrl(undefined);
-        }}>Unload</button> 
-      </main>
-      :
-      <DropZone onDrop={async (file) => {
-        setUrl(URL.createObjectURL(new Blob([await file.arrayBuffer()])));
-        setName(file.name);
-      }} />
-  )
+    <>
+      <Head>
+        <title>{name ?? "Keynote inspector"}</title>
+      </Head>
+      {url ? (
+        <main className="flex min-h-screen flex-col items-center justify-start p-24">
+          <h1 className="font-bold">
+            <Image
+              alt="icon"
+              className="inline"
+              src="/keynote.png"
+              width="32"
+              height="32"
+            />
+            &nbsp;{name}&nbsp;
+            <button
+              className="bg-gray-500 hover:bg-gray-700 text-white font-bold py-2 px-4 rounded-full"
+              onClick={() => {
+                localStorage.setItem("name", undefined!);
+                localStorage.setItem("url", undefined!);
+                setName(undefined);
+                setUrl(undefined);
+              }}
+            >
+              Unload
+            </button>
+          </h1>
+        </main>
+      ) : (
+        <DropZone
+          onDrop={async (file) => {
+            setUrl(URL.createObjectURL(new Blob([await file.arrayBuffer()])));
+            setName(file.name);
+          }}
+        />
+      )}
+    </>
+  );
 }
